e5117362b3aaff6109d5ec148f55f2012f2411b6
vaibhav <chandravaibhav65@gmail.com>
2021-04-21 00:53:13 +0530

    scheduled jobs complete

--- scheduledJobs.js
+++ scheduledJobs.js
@@ -11,22 +11,51 @@ const cquery = async  (sql,req,res)=>{
     )
 }
 
-schedule.scheduleJob('0 0 0 * * *', async ()=>{
-    let year = new Date().getFullYear();
-    let month = new Date().getMonth()+1;
-    let date = new Date().getDate();
-    if(date <10){
-        date = '0'+date;
-    }
-    if(month<10){
-        month = '0'+month;
-    }
-    let cdate = `${year}-${month}-${date}`;
-    let emails = await cquery(`select email from user where userID in (select userID from bookCopiesUser where (ISBN,copyID) in (select ISBN,copyID from bookCopies where dueDate > '${cdate}'));`);
-    let len = emails.length;
-    console.log(emails);
+schedule.scheduleJob('0 0 2 * * *', async ()=>{
+    let copies=await cquery(`select * from bookCopies;`);
+    let len = copies.length;
     for(let i=0;i<len;i++){
-    } 
+        await cquery(`call cancelHold('${copies[i].ISBN}',${copies[i].copyID},@status);`);
+        let status = await cquery(`select @status;`);
+        if(status[0]['@status']==1){
+            console.log('hold revoked');
+        }else{
+            console.log('hold not revoked');
+        }
+    }
+})
+
+schedule.scheduleJob('0 0 0 * * *', async ()=>{
+    let users = await cquery(`select userID from bookCopiesUser;`);
+    let prev=-1;
+    let len=users.length;
+    for(let i =0 ;i<len;i++){
+        if(prev==users[i].userID){
+            continue;
+        }
+        let email = await cquery(`select email from user where userID=${users[i].userID};`);
+        let temp = await cquery(`call emailDetails(${users[i].userID},@unpaidFine);`);
+        // console.log(temp);
+        let fine = await cquery(`select @unpaidFine;`);
+        fine = fine[0]['@unpaidFine'];
+        let tlen=temp[0].length;
+        // console.log(tlen);
+        let mess="";
+        for(let j=0;j<tlen;j++){
+            let action = temp[0][j].action;
+            let cur = temp[0][j];
+            if(action == 'hold'){
+                mess+=`<p>Your hold request for the book '${cur.title}', ISBN: ${cur.ISBN}, copyID: ${cur.copyID} has expired on ${cur.dueDate}. Your unpaid fines are Rs. ${fine}.</p>`;
+            }else if(action == 'loan&hold'){
+                mess+=`<p>Your loan&hold on the book '${cur.title}', ISBN: ${cur.ISBN}, copyID: ${cur.copyID} has expired on ${cur.dueDate}. Please return it to the library as soon as possible to avoid getting fined. Your unpaid fines are Rs. ${fine}.</p>`;
+            }else if(action == 'loan'){
+                mess+=`<p>Your loan on the book '${cur.title}', ISBN: ${cur.ISBN}, copyID: ${cur.copyID} has expired on ${cur.dueDate}. Please renew it or return it to library as soon as possible to avoid getting fined. Your unpaid fines are Rs. ${fine}.</p>`;
+            }
+            // mail('Due Date Expired',mess,cur.email);
+        }
+        mail('Due Date Expired',mess,email[0].email);
+        prev=users[i].userID;
+    }
 })
 
 schedule.scheduleJob('0 0 0 * * *',async ()=>{

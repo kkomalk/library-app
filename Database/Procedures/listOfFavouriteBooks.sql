237a89b0182f175ede7a6206ffee95b64f4ab342
vaibhav <chandravaibhav65@gmail.com>
2021-04-20 15:16:49 +0530

    user home complete 2

--- Database/Procedures/listOfFavouriteBooks.sql
+++ Database/Procedures/listOfFavouriteBooks.sql
@@ -7,7 +7,7 @@ create procedure listOfFavouriteBooks(
 begin
 create table temp
 select readingList.ISBN, readingList.userID from readingList where readingList.userID = userID and readingList.favourite = 'YES';
-select temp.ISBN, user.name, book.title, book.yearOfPublication, book.authors, book.category, book.image 
+select temp.ISBN, user.name, user.userID, book.title, book.yearOfPublication, book.authors, book.category, book.image 
 from temp inner join book inner join user
 on book.ISBN = temp.ISBN and temp.userID = user.userID;
 drop table temp;
